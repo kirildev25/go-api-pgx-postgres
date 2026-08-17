775365461da2d7ab5b0ff673d47f89720c343b3e
Kiril Dev <301549652+kirildev25@users.noreply.github.com>
2026-08-17 13:27:12 +0000

    get workout by id

--- internal/app/app.go
+++ internal/app/app.go
@@ -32,9 +32,10 @@ func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// stores
+	workoutStore := store.NewPostgresWorkoutStore(pgDB)
 
 	// handlers
-	workoutHandler := api.NewWorkoutHandler()
+	workoutHandler := api.NewWorkoutHandler(workoutStore)
 
 	app := &Application{
 		Logger:         logger,
