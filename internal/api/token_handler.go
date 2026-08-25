29beab0679e396383e43437c4b1cdf7e71752131
Kiril Dev <301549652+kirildev25@users.noreply.github.com>
2026-08-25 08:37:14 +0000

    add app to api token

---
+++ internal/api/token_handler.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/kirildev25/go-api-pgx-postgres/internal/store"
+	"github.com/kirildev25/go-api-pgx-postgres/internal/tokens"
+	"github.com/kirildev25/go-api-pgx-postgres/internal/utils"
+)
+
+type TokenHandler struct {
+	tokenStore store.TokenStore
+	userStore  store.UserStore
+	logger     *log.Logger
+}
+
+type createTokenRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func NewTokenHandler(tokenStore store.TokenStore, userStore store.UserStore, logger *log.Logger) *TokenHandler {
+	return &TokenHandler{
+		tokenStore: tokenStore,
+		userStore:  userStore,
+		logger:     logger,
+	}
+}
+
+func (h *TokenHandler) HandleCreateToken(w http.ResponseWriter, r *http.Request) {
+	var req createTokenRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		h.logger.Printf("ERROR: create token request: %v", err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "invalid request payload"})
+		return
+	}
+
+	user, err := h.userStore.GetUserByUsername(req.Username)
+	if err != nil || user == nil {
+		h.logger.Printf("ERROR: GetUserByUsername: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	passwordsDoMatch, err := user.PasswordHash.Matches(req.Password)
+	if err != nil {
+		h.logger.Printf("ERROR: PasswordHash.Matches: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	if !passwordsDoMatch {
+		utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "invalid credentials"})
+	}
+
+	token, err := h.tokenStore.CreateNewToken(user.ID, 24*time.Hour, tokens.ScopeAuth)
+	if err != nil {
+		h.logger.Printf("ERROR: CreateNewToken: %v", err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "invalid request payload"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, utils.Envelope{"auth_token": token})
+}
